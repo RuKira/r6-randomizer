096cf2f399b44125e8f04fca32b0d7e6624c7be5
RuKira <jduesling13@gmail.com>
2025-08-16 21:24:55 -0400

    Test #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import OperatorRandomizerUI from './OperatorRandomizerUI';
-import Overlay from './Overlay';
 
-function App() {
-  return (
-    <Router basename="/r6-randomizer">
-      <Routes>
-        <Route path="/" element={<OperatorRandomizerUI />} />
-        <Route path="/overlay" element={<Overlay />} />
-      </Routes>
-    </Router>
-  );
+export default function App() {
+  return <OperatorRandomizerUI />;
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import OperatorRandomizerUI from './App';
-import './index.css';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Overlay from "./Overlay";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <OperatorRandomizerUI />
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter basename="/">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/overlay" element={<Overlay />} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
 );
