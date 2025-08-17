6fb0534b902ab4a1efeeaad2bec84335322d4ae8
RuKira <jduesling13@gmail.com>
2025-08-16 21:19:40 -0400

    Twitch Overlay

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import OperatorRandomizerUI from './OperatorRandomizerUI';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import OperatorRandomizerUI from './OperatorRandomizerUI';
+import Overlay from './Overlay';
 
 function App() {
-    return <OperatorRandomizerUI />;
+  return (
+    <Router basename="/r6-randomizer">
+      <Routes>
+        <Route path="/" element={<OperatorRandomizerUI />} />
+        <Route path="/overlay" element={<Overlay />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
